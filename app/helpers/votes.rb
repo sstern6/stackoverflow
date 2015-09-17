ccb28f221533e09fbd5e52dfe5e8142479514b0f
Galen Cook <galenscook@gmail.com>
2015-09-16 20:45:59 -0700

    Add first round vote buttons

--- app/helpers/votes.rb
+++ app/helpers/votes.rb
@@ -1,7 +1,7 @@
 helpers do 
 
   def current_vote(voteableid, votabletype) 
-    Vote.find_by(user_id: current_user.id, voteable_id: voteableid, votable_type: votabletype)
+    Vote.find_by(user_id: current_user.id, voteable_id: voteableid, voteable_type: votabletype)
   end
   
 end
